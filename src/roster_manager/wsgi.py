3dc3466f8413ed9157e0a13cdae27352702f5cb7
vasu-suraasa <vasu@suraasa.com>
2024-10-18 23:18:50 +0530

    init roster-manager

--- src/roster_manager/wsgi.py
+++ src/roster_manager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for roster_pulse project.
+WSGI config for roster_manager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roster_pulse.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roster_manager.settings")
 
 application = get_wsgi_application()
